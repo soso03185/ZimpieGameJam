9251fe301284667a4b7eef309da30f7fdd3f04e7
wdmab1204 <wdmab1204@naver.com>
2023-02-04 00:33:49 +0900

    범위가 넓을수록 스폰쿨타임이 올라가도록 수정

--- Assets/PKW_script/Stage/RandomItemSpawnSystem.cs
+++ Assets/PKW_script/Stage/RandomItemSpawnSystem.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class RandomItemSpawnSystem : MonoBehaviour
 {
-    [Range(0, 100)] public int randomPositionRange = 10;
+    [Range(0, 20)] public int randomPositionRange = 10;
     [SerializeField] private GameObject itemPrefab;
-    [SerializeField] private float spawnInterval = 0.5f;
+    //[SerializeField] private float spawnInterval = 0.5f;
+    private float k = 5.0f;
 
     private void Start()
     {
@@ -20,7 +21,7 @@ public class RandomItemSpawnSystem : MonoBehaviour
             var objClone = Instantiate(itemPrefab);
             float randomPositionX = Random.Range(-randomPositionRange / 2, randomPositionRange / 2);
             objClone.transform.position = new Vector2(this.transform.position.x + randomPositionX, this.transform.position.y);
-            yield return new WaitForSeconds(spawnInterval);
+            yield return new WaitForSeconds(k / randomPositionRange);
         }
     }
 
