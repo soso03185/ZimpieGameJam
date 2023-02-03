a4cc70f5ae81304f94e64d91621f9f3a98fce620
soso03185 <34054994+soso03185@users.noreply.github.com>
2023-02-03 23:21:59 +0900

    UI_1

---
+++ Assets/KJH/KJH_Scripts/KJH_StageButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KJH
+{
+    public class KJH_StageButton : MonoBehaviour
+    {
+        public int StageIndex;       
+
+        public void StageButton()
+        {
+            
+
+        }
+
+    }
+
+}
