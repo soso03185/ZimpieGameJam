90ebf8310446de5f1c5a15963fc0f08fb03a6a0c
soso03185 <34054994+soso03185@users.noreply.github.com>
2023-02-04 08:56:31 +0900

    HpBar_ing

--- Assets/KJH/KJH_Scripts/KJH_HpBar.cs
+++ Assets/KJH/KJH_Scripts/KJH_HpBar.cs
@@ -2,21 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class KJH_HpBar : MonoBehaviour
 {
-    public Text myText;
     public Image Hp_Bar;
-    
     float damageTime;
-    float timeCount = 0;
+
+    bool isGreen;
+    bool isYellow;
+    bool isRed;
 
     public void Update()
     {
-        timeCount += Time.deltaTime;
-        myText.text = timeCount.ToString();
-
         damageTime = Time.deltaTime / 15;
         Hp_Bar.fillAmount -= damageTime;
+
+        // 0 ~ 20 /  20 ~ 80 / 80 ~ 100
+        if(Hp_Bar.fillAmount < 0.8f)
+        {
+            isGreen = false;
+            isYellow = true;
+            isRed = false;
+        }
+        else if (Hp_Bar.fillAmount < 0.2f)
+        {
+            isGreen = false;
+            isYellow = false;
+            isRed = true;
+        }
+        else
+        {
+            isGreen = true;
+            isYellow = false;
+            isRed = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Hp_Bar.DOColor(new Color(255, 0, 0), 1);    // red
+         //   Hp_Bar.DOColor(new Color(255, 192, 0), 1);  // yellow
+         //   Hp_Bar.DOColor(new Color(176, 80, 0), 1);   // green
+        }
     }
+
 }
