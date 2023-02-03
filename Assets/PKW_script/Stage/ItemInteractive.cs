6739ac611e2adca07e07d3eae72247e5cc155444
wdmab1204 <wdmab1204@naver.com>
2023-02-04 08:46:38 +0900

    플레이어 속도 조정

--- Assets/PKW_script/Stage/ItemInteractive.cs
+++ Assets/PKW_script/Stage/ItemInteractive.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ItemInteractive : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.transform.CompareTag("Player") || collision.transform.CompareTag("Ground"))
         {
