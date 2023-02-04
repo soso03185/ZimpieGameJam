fc3b97c75c9ace720b5b290108f05643c9a8d05c
soso03185 <34054994+soso03185@users.noreply.github.com>
2023-02-05 01:23:47 +0900

    1

--- Assets/KJH/KJH_Scripts/KJH_StageButton.cs
+++ Assets/KJH/KJH_Scripts/KJH_StageButton.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace KJH
 {
@@ -9,6 +10,60 @@ namespace KJH
     {
         public string StageName;
         public int StageIndex;
+
+        public GameObject CureStartImage;
+        public GameObject FaceImage;
+        public GameObject PassImage;
+        public Button PanelButton;
+
+        private void Start()
+        {
+            GameObject go = GameObject.Find("DataManager");
+            DataManager DM = go.GetComponent<DataManager>();
+
+            Debug.Log("0");
+
+            if (DM.ClearStage_1 && StageIndex == 1)
+            {
+                PanelButton.enabled = false;
+                FaceImage.SetActive(false);
+                CureStartImage.SetActive(true);
+                PassImage.SetActive(true);
+                Debug.Log("1");
+            }
+            else if (DM.ClearStage_2 && StageIndex == 2)
+            {
+                PanelButton.enabled = false;
+                FaceImage.SetActive(false);
+                CureStartImage.SetActive(true);
+                PassImage.SetActive(true);
+                Debug.Log("2");
+            }
+            else if (DM.ClearStage_3 && StageIndex == 3)
+            {
+                PanelButton.enabled = false;
+                FaceImage.SetActive(false);
+                CureStartImage.SetActive(true);
+                PassImage.SetActive(true);
+                Debug.Log("3");
+            }
+            else if (DM.ClearStage_4 && StageIndex == 4)
+            {
+                PanelButton.enabled = false;
+                FaceImage.SetActive(false);
+                CureStartImage.SetActive(true);
+                PassImage.SetActive(true);
+                Debug.Log("4");
+            }
+            else if (DM.ClearStage_5 && StageIndex == 5)
+            {
+                PanelButton.enabled = false;
+                FaceImage.SetActive(false);
+                CureStartImage.SetActive(true);
+                PassImage.SetActive(true);
+                Debug.Log("5");
+            }
+        }
 
         public void StageButton()
         {
@@ -18,6 +73,11 @@ namespace KJH
         public void StartButton()
         {
             SceneManager.LoadScene("KJH_StoryScene");
+        }
+
+        public void StageClear()
+        {
+
         }
     }
 }
