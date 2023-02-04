267e3168ecded1b2a03a09da24f172026cc501ae
wdmab1204 <wdmab1204@naver.com>
2023-02-04 12:42:31 +0900

    Pause 팝업 윈도우

---
+++ Assets/04 Scripts/PausePanel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class PausePanel : MonoBehaviour
+{
+    public Button continueBtn;
+    public Button restartBtn;
+    public Button exitBtn;
+
+    public void Continue()
+    {
+        //GameManager.Instance.AnimatingPausePanel(true);
+    }
+
+    public void Restart()
+    {
+        //버그가많다
+        Time.timeScale = 1.0f;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(gameObject.scene.name);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    private void Start()
+    {
+        continueBtn.onClick.AddListener(Continue);
+        restartBtn.onClick.AddListener(Restart);
+        exitBtn.onClick.AddListener(Exit);
+    }
+}
--- Assets/PKW_script/Stage/GameManager.cs
+++ Assets/PKW_script/Stage/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class GameManager : MonoBehaviour
 {
@@ -53,5 +54,81 @@ public class GameManager : MonoBehaviour
         Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
         Debug.Log("Scene reload currentScene");
     }
-    
+
+    [Space(10)]
+    [SerializeField] private int stoneCount;
+    [SerializeField] private GameObject stonePrefab;
+    public enum SpeedState
+    {
+        SLOW,
+        NORMAL,
+        FAST
+    };
+    [SerializeField] private SpeedState speedState = SpeedState.FAST;
+    [SerializeField] private GameObject itemPrefab;
+    private void SpawnStone()
+    {
+        for (int i = 0; i < stoneCount; i++)
+        {
+            var objClone = Instantiate(stonePrefab);
+
+            float randomX = Random.Range(-9.0f, 9.0f);
+            objClone.transform.position = new Vector3(randomX, objClone.transform.position.y);
+        }
+    }
+
+    private void ChangeItemSpeed()
+    {
+        switch (speedState)
+        {
+            case SpeedState.SLOW:
+                itemPrefab.GetComponent<Rigidbody2D>().gravityScale = 0.5f;
+                break;
+            case SpeedState.NORMAL:
+                itemPrefab.GetComponent<Rigidbody2D>().gravityScale = 1.0f;
+                break;
+            case SpeedState.FAST:
+                itemPrefab.GetComponent<Rigidbody2D>().gravityScale = 1.5f;
+                break;
+        }
+    }
+
+    bool isOpened = false;
+    Tweener tweenner;
+    [SerializeField] private PausePanel pausePanel;
+    public void AnimatingPausePanel(bool onoff)
+    {
+        //if (onoff)
+        //{
+        //    tweenner.Kill();
+        //    pausePanel.transform.DOScale(new Vector3(0, 0, 0), 0.25f).SetEase(Ease.InOutExpo);//.OnComplete(() => pausePanel.gameObject.SetActive(false));
+
+        //    Time.timeScale = 1.0f;
+
+        //}
+        //else
+        //{
+
+        //    tweenner.Kill();
+        //    pausePanel.transform.DOScale(new Vector3(1, 1, 1), 0.5f).SetEase(Ease.InExpo).SetEase(Ease.OutBounce).SetUpdate(true);
+
+        //    Time.timeScale = 0.0f;
+        //}
+    }
+
+    private void Start()
+    {
+        SpawnStone();
+        ChangeItemSpeed();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            AnimatingPausePanel(isOpened);
+            isOpened = !isOpened;
+        }
+    }
+
 }
